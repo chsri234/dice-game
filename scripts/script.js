01ef5468c35175a460ae9c5f5d2505a9c467062e
chsri234 <chalamcharlasrikanthreddy@gmail.com>
2020-12-28 09:35:15 +0530

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
+// if(randomNumber1 === 1) {
+//   document.querySelector('.imagePlayer1').setAttribute('src', 'images/dice1.png');
+// }else if(randomNumber1 === 2) {
+//     document.querySelector('.imagePlayer1').setAttribute('src', 'images/dice2.png');
+// }else if(randomNumber1 === 3) {
+//     document.querySelector('.imagePlayer1').setAttribute('src', 'images/dice3.png');
+// }else if(randomNumber1 === 4) {
+//     document.querySelector('.imagePlayer1').setAttribute('src', 'images/dice4.png');
+// }else if(randomNumber1 === 5) {
+//     document.querySelector('.imagePlayer1').setAttribute('src', 'images/dice5.png');
+// }else {
+//     document.querySelector('.imagePlayer1').setAttribute('src', 'images/dice6.png');
+// }
+var randomImage1 = 'images/dice' + randomNumber1 + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src', randomImage1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2);
+// if(randomNumber2 === 1) {
+//   document.querySelector('.imagePlayer2').setAttribute('src', 'images/dice1.png');
+// }else if(randomNumber2 === 2) {
+//     document.querySelector('.imagePlayer2').setAttribute('src', 'images/dice2.png');
+// }else if(randomNumber2 === 3) {
+//     document.querySelector('.imagePlayer2').setAttribute('src', 'images/dice3.png');
+// }else if(randomNumber2 === 4) {
+//     document.querySelector('.imagePlayer2').setAttribute('src', 'images/dice4.png');
+// }else if(randomNumber2 === 5) {
+//     document.querySelector('.imagePlayer2').setAttribute('src', 'images/dice5.png');
+// }else {
+//     document.querySelector('.imagePlayer2').setAttribute('src', 'images/dice6.png');
+// }
+
+var randomImage2 = 'images/dice' + randomNumber2 + '.png';
+document.querySelectorAll('img')[1].setAttribute('src', randomImage2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = '🚩 Player 1 Wins!';
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = 'Player 2 Wins! 🚩';
+} else {
+    document.querySelector('h1').innerHTML = 'Draw!';
+}
